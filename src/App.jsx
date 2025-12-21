773b768b2131d43cefbb4397e1085209adbe989e
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-12-21 17:48:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -17,6 +16,7 @@ function App() {
       <Projects />
       <Certifications />
       <Skills />
+      <Analytics />
     </div>
   )
 }
